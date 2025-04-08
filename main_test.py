c7cdbfa7547564fe5edb3aac83fb0f91072c3328
Filipp Voronov <fivoronov@gmail.com>
2025-04-08 20:01:57 +0300

    Break test

--- main_test.py
+++ main_test.py
@@ -4,5 +4,5 @@ from main import find_min_price
 def test():
     prices = [200, 150, 118, 211, 108, 561]
     actual = find_min_price(prices)
-    expected = 108
+    expected = 109
     assert expected == actual
