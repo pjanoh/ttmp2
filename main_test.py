81bc2263472d73aa24f9216c64a6d4c8efaec58c
Filipp Voronov <fivoronov@gmail.com>
2025-04-08 20:00:54 +0300

    Linter applied

--- main_test.py
+++ main_test.py
@@ -1,7 +1,8 @@
 from main import find_min_price
 
+
 def test():
-    prices = [200, 150, 118, 211, 108, 560]
+    prices = [200, 150, 118, 211, 108, 561]
     actual = find_min_price(prices)
     expected = 108
     assert expected == actual
